2684177d94c76ea79149edd42fc6ed89a7b72e24
A-2809 <66512619+A-2809@users.noreply.github.com>
2020-06-10 14:49:17 +0530

    Add files via upload

--- LCM.cpp
+++ LCM.cpp
@@ -4,11 +4,13 @@ int HCF(int x,int y);
 int main()
 {
 	int i,j,z,LCM;
-	printf("enter any two integers: ");
-	scanf("%d %d",&i,&j);
+	printf("Input first number:");
+	scanf("%d",&i);
+	printf("Input second number: ");
+	scanf("%d",&j);
 	z=HCF( i,j);
 	LCM=(i*j)/z;
-	printf("LCM of %d and %d is %d.",i,j,LCM);
+	printf("LCM of %d and %d = %d.",i,j,LCM);
 	return 0;
 }
 int HCF(int x,int y)
